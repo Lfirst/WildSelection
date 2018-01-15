ca7faf6412f53f97dbbc6390b5f96f4bd9fa5014
Blatantbeast <35450175+Blatantbeast@users.noreply.github.com>
2018-01-15 11:12:24 +0100

    Add files via upload

---
+++ Unity/Assets/Personnages/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator anim;
+    private float forward;
+    private float backward;
+    private float run1;
+    private float walkR;
+    private float walkL;
+    private float jump;
+    private float Idle;
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // jump :
+
+        jump = Input.GetAxis("Jump");
+        anim.SetFloat("jump", jump);
+
+        //walk right :
+
+        walkR = Input.GetAxis("Horizontal");
+        if (walkR < 0)
+        {
+            walkR = 0;
+        }
+        anim.SetFloat("walkR", walkR);
+
+        //walk left :
+
+        walkL = Input.GetAxis("Horizontal");
+        if (walkL > 0)
+        {
+            walkL = 0;
+        }
+        anim.SetFloat("walkL", walkL);
+
+        // run :
+
+        run1 = Input.GetAxis("Run");
+        run1 *= forward;
+        anim.SetFloat("run", run1);
+
+        //forward :
+
+        forward = Input.GetAxis("Vertical");
+        if (walkL < 0 || walkR > 0)
+        {
+            forward = 0;
+        }
+        anim.SetFloat("forward", forward);
+
+        //fin Idle :
+        Idle = jump + forward + walkR - walkL + run1;
+        anim.SetFloat("Idle", Idle);
+    }
+}
